3b1e73875ed04a67c71d19bbbfb24f3f065dc220
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-16 14:29:27 -0400

    Updated from Brython Server: 10/16/2017 2:29:26 PM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,9 @@
+#Matthew Siegel
+#10/16/17
+#funcitonDemo.py - learning functions
 
+def rectangle(len,wid):
+    print('There area is',len*wid)
+    print('The perimenter is',len*2 + wid*2)
+    
+rectangle(3,4)
