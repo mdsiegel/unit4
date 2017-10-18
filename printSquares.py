333685aa2de2c58241a009e5f2ae85433414be13
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-18 13:46:27 -0400

    Updated from Brython Server: 10/18/2017 1:46:28 PM

--- printSquares.py
+++ printSquares.py
@@ -2,8 +2,16 @@
 #10/16/17
 #printSquares.py - Makes a bunch of boxes
 
-def printSquares(hight,width):
-    for i in range(1,
+def printSquares(height,width):
+    for i in range(1,height+1):
+        print('+--'*width+'+')
+        print('|  '*width+'|')
+    print('+--'*width+'+')
+        
+            
+        
+        
+    
 
-printSquares(1,2)
+printSquares(3,5)
     
