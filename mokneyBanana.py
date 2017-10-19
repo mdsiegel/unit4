9d51ee5a3376c2a4379d2267d2f9a5a9a7fe90fd
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-19 14:13:49 -0400

    Updated from Brython Server: 10/19/2017 2:13:52 PM

--- mokneyBanana.py
+++ mokneyBanana.py
@@ -3,12 +3,17 @@
 #mokneyBanana.py - best game ever
 
 from ggame import *
+#constants
+ROWS = 20
+COLLS = 40
+CELL_SIZE = 20
+
 
 if __name__ == '__main__':
     
     green = Color(0x006600,1)
     
-    jungleBox = RectangleAsset(800,600,LineStyle(1,green),green)
+    jungleBox = RectangleAsset(COLLS*CELL_SIZE,ROWS*CELL_SIZE,LineStyle(1,green),green)
     
     Sprite(jungleBox)
     
