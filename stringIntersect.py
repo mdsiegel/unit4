a179dccc77e0f3d9cada1e33180887b55b1a2738
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-20 14:51:09 -0400

    Updated from Brython Server: 10/20/2017 2:51:11 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,21 @@
+#Matthew Siegel
+#10/19/17
+#stringIntersect.py - Gets a string from the original
+
+def stringIntersect(str1,str2):
+    totalStr = ''
+    totalStr += str1
+        
+    totalStr +=str2
+    finalStr = ''
+    for ch in totalStr:
+        if ch in str1 and ch in str2:
+            if not ch in finalStr:
+                finalStr += ch
+        
+            
+            
+    return finalStr
+    
+print(stringIntersect('mississippi','pennsyvania'))
 
