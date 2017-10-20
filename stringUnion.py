4cfc334b4e41636b6bab5876f758d2685726ab80
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-20 14:42:43 -0400

    Updated from Brython Server: 10/20/2017 2:42:45 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,17 @@
+#Matthew Siegel
+#10/19/17
+#stringUnion.py - Gets a string from the original
+
+def stringUnion(str1,str2):
+    totalStr = ''
+    totalStr += str1
+        
+    totalStr +=str2
+    for ch in totalStr:
+        
+            
+    return totalStr
+    
+print(stringUnion('hi','hey'))
+    
 
