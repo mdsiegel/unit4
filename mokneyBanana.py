8ca70bc7fc28fc0674d7e5bc35de5ab49036897f
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-20 14:22:53 -0400

    Updated from Brython Server: 10/20/2017 2:22:54 PM

--- mokneyBanana.py
+++ mokneyBanana.py
@@ -37,18 +37,27 @@ def moveDown(event):
 def moveBanana():
     banana.x = randint(0,COLLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
+    data['frames'] = 0
     
 def updateScore():
     data['score'] +=1000
     data['scoreText'].destroy()
-    scorebox = TextAsset('Score = '+str(data['score']))
+    scoreBox = TextAsset('Score = '+str(data['score']))
     data['scoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
     
+def step():
+    data['frames'] +=1
+    if data['frames'] == 1:
+        print('time')
+        moveBanana()
+
 
 if __name__ == '__main__':
     
     data= {}
     data['score'] = 0
+    data['frames'] = 0
+    
     green = Color(0x006600,1)
     brown = Color(0x8B4513,1)
     yellow = Color(0xFFFF00,1)
@@ -70,4 +79,4 @@ if __name__ == '__main__':
 
 
 
-    App().run()
+    App().run(step)
