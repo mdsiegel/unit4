7b5537e63d1a171ecc9e8031edb2e40a348e3d5b
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-23 14:48:26 -0400

    Updated from Brython Server: 10/23/2017 2:48:29 PM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,8 @@
+#Matthew Siegel
+#10/23/17
+#trianlge.py - Finding the area of a triangle
 
+def distance(x1,y1,x2,y2):
+    distance = ((x2-x1)**2+(y2-y1)**2)**(1/2)
+    return distance
+print(distance(1,0,20,0))
