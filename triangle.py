727d818b621b7b5f6d7eb8b90255f78781da9fbc
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-23 14:51:09 -0400

    Updated from Brython Server: 10/23/2017 2:51:12 PM

--- triangle.py
+++ triangle.py
@@ -2,7 +2,19 @@
 #10/23/17
 #trianlge.py - Finding the area of a triangle
 
+x1 = float(input('Enter the x1 value: '))
+y1 = float(input('Enter the y1 value: '))
+x2 = float(input('Enter the x2 value: '))
+y2 = float(input('Enter the y2 value: '))
+x3 = float(input('Enter the x3 value: '))
+y3 = float(input('Enter the y3 value: '))
+
 def distance(x1,y1,x2,y2):
-    distance = ((x2-x1)**2+(y2-y1)**2)**(1/2)
-    return distance
-print(distance(1,0,20,0))
+    a = ((x2-x1)**2+(y2-y1)**2)**(1/2)
+    return a
+def distance(x1,y1,x2,y2):
+    b = ((x3-x2)**2+(y3-y2)**2)**(1/2)
+    return b
+def distance(x1,y1,x2,y2):
+    c = ((x3-x1)**2+(y3-y1)**2)**(1/2)
+    return b
