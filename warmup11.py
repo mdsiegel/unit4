22497706f243dfb8a4091b0f00dd057255508d27
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-23 14:21:14 -0400

    Updated from Brython Server: 10/23/2017 2:21:16 PM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,12 @@
+#Matthew Siegel
+#10/23/17
+#warmup11.py - Determine if a number is prime
+
+def isPrime(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return false
+        else:
+            return true
+            
 
