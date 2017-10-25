23a53e2f3f1c5f561f75407d0d112047c9305f64
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-25 13:42:36 -0400

    Updated from Brython Server: 10/25/2017 1:42:43 PM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,13 @@
+#Matthew Siegel
+#10/25/17
+#multiply.py - multiplying game
+
+from random import randint
+counter = 0
+while true:
+    num1 = randint(1,10)
+    num2 = randint(1,10)
+    if int(input('What is',num1,'*',num2,'? ')) == num1*num2:
+        counter+=1
+    
 
