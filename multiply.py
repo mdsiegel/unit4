ce64cfb7d12d3654b1a7e2ad8e9d06cbecb38aa2
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-25 13:45:56 -0400

    Updated from Brython Server: 10/25/2017 1:46:03 PM

--- multiply.py
+++ multiply.py
@@ -4,10 +4,25 @@
 
 from random import randint
 counter = 0
-while true:
+while True:
     num1 = randint(1,10)
     num2 = randint(1,10)
     if int(input('What is',num1,'*',num2,'? ')) == num1*num2:
         counter+=1
+    if counter == 5:
+        statement()
     
-
+def statement():
+    randnum = drandint(1,5)
+    if randnum == 1:
+        print('You got this keep going')
+    elif randnum == 2:
+        print('You are doing better than Gary!')
+    elif randnum == 3:
+        print('You are doing better than Eric!')
+    elif randnum == 4:
+        print('NAILED IT')
+    elif randnum == 5:
+        print('Keep going!')
+    
+    
