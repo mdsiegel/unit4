6db9b6dd6b61665b64050cbb7e7399875517e890
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-26 14:45:20 -0400

    Updated from Brython Server: 10/26/2017 2:45:25 PM

--- sunrise.py
+++ sunrise.py
@@ -1,1 +1,19 @@
+#Matthew Siegel
+#10/25/17
+#sunrise.py - making a sunrise
 
+def step():
+    ball.y +=3
+
+if __name__ == '__main__':
+    yellow = Color(0xFDB813,1)
+    yellowOutline = LineStyle(1,yellow) #pixels,color
+    
+    circle = CircleAsset(50, yellowOutline, yellow)
+    ball = Sprite(circle,(20,20))
+    
+    data = {}
+    data['directionx'] = 20
+    data['directiony'] = 20
+    
+    App().run(step)
