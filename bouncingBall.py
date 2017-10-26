304c4ae54f09b45e48f71743c97388f584e12e51
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-26 14:26:20 -0400

    Updated from Brython Server: 10/26/2017 2:26:23 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -6,13 +6,17 @@ from ggame import *
 from random import randint
 
 green = Color(0x00FF00,1)
+black = Color(0x000000,1)
+
 blackOutline = LineStyle(1,black) #pixels,color
-circle = CircleAsset(randint(20,100), blackOutline, green)
+circle = CircleAsset(50, blackOutline, green)
 Sprite(circle,(20,20))
 
-def step():
-    circle.x+=1
-    circle.y+=1
-App().run()
 
+def step():
+    circle.x+=10
+    circle.y+=10
+    print('step')
+
+App().run(step)
 
