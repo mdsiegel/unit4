e66d69466ae14d9e2aa145e2cfe9224b92f86fd2
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-03-30 08:40:47 -0400

    Updated from Brython Server: 3/30/2018 8:40:48 AM

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,11 @@
+#Matthew Siegel
+#3/30/18
+#fibonacci.py - Making the fibonacci sequence
 
+def fibonacci(n):
+    if n == 2 or n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+        
+fibonacci(10)
