8d8954d9344c8e8c4d114cc5a53d53ddc226d3e1
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-02 08:19:49 -0400

    Updated from Brython Server: 4/2/2018 8:19:51 AM

--- quiz4.py
+++ quiz4.py
@@ -2,13 +2,22 @@
 #4/2/18
 #quiz4.py - Learning how to do a quiz
 
-def countsUp(int):
+def count(int):
     for i in range(1,int+1):
         print(i)
         
-countsUp(10)
+count(10)
 
 def excitedPrint(string):
-    string = string.upper
-    print(
+    string = string.upper()
+    string = string + "!!!"
+    print(string)
 excitedPrint("hi")
+
+def firstLetter(string):
+    i = 1
+    for ch in string:
+        if i == 1:
+            print(ch)
+        i+=1
+firstLetter("does this work")
