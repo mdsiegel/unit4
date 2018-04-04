2847f35d035a5f6ac70fd34c7686967fddcf15c3
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-04 08:36:06 -0400

    Updated from Brython Server: 4/4/2018 8:36:09 AM

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,19 @@
+#Matthew Siegel
+#4/4/18
+#Hangman.py - hangman game
 
+from ggame import *
+
+black = Color(0x000000,1)
+
+def pickWord():
+    word == "word"
+
+def wordComplete():
+    
+def printHangman():
+    
+def keyPress(event):
+    
+if __name__ == '__main__':
+    
