1f7c7fd1ea2bbdf714c8e6ea3236bfb385121702
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-30 08:22:47 -0400

    Updated from Brython Server: 4/30/2018 8:22:50 AM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,18 @@
+#Matthew Siegel
+#4/30/18
+#warmup14.py - telling the initials of a name
+
+name = input('What is your first and last name: ').split(' ')
+
+i = 0
+for w in name[0]:
+    if i == 0:
+        firstInitial = w
+
+b = 0
+for ch in name[1]:
+    if b == 0:
+        firstInitial = ch
+        
+print(w,ch)
 
