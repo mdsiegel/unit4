c176e17db4bb7c784ea821968b7333a6e4b98a50
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-30 08:06:24 -0400

    Updated from Brython Server: 4/30/2018 8:06:26 AM

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,13 @@
+#Matthew Siegel
+#4/30/18
+#antsDemo.py - using lists with graphics
 
+from ggame import *
+
+ANTS = 10
+
+if __name__ == '__main__':
+    
+    red = Color(0xFF0000,1)
+    ant = CirlceAsset(20
+    
